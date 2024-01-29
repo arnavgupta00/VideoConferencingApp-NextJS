c16b133cea6bb66f15b186d08ceaf20efe839df6
arnavgupta00 <arnavguptagg@gmail.com>
2024-01-29 17:42:02 +0530

    added sitemal xml again

--- src/app/sitemap/page.tsx
+++ src/app/sitemap/page.tsx
@@ -1,34 +1,28 @@
-const siteMap = `<?xml version="1.0" encoding="UTF-8"?>
-<urlset
-            xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
-            xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-            xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9
-                        http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
-<!-- created with Free Online Sitemap Generator www.xml-sitemaps.com -->
-
-
+const page = () => {
+    const siteMap = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 <url>
-    <loc>https://gragie.arnavgupta.tech/</loc>
-    <lastmod>2024-01-29T11:55:27+00:00</lastmod>
-    <priority>1.00</priority>
+        <loc>https://gragie.arnavgupta.tech/</loc>
+        <lastmod>2024-01-29T11:55:27+00:00</lastmod>
+        <priority>1.00</priority>
 </url>
 <url>
-    <loc>https://gragie.arnavgupta.tech/roomCreate</loc>
-    <lastmod>2024-01-29T11:55:27+00:00</lastmod>
-    <priority>0.80</priority>
+        <loc>https://gragie.arnavgupta.tech/roomCreate</loc>
+        <lastmod>2024-01-29T11:55:27+00:00</lastmod>
+        <priority>0.80</priority>
 </url>
 <url>
-    <loc>https://gragie.arnavgupta.tech/roomJoin</loc>
-    <lastmod>2024-01-29T11:55:27+00:00</lastmod>
-    <priority>0.80</priority>
+        <loc>https://gragie.arnavgupta.tech/roomJoin</loc>
+        <lastmod>2024-01-29T11:55:27+00:00</lastmod>
+        <priority>0.80</priority>
 </url>
 <url>
-    <loc>https://gragie.arnavgupta.tech/callRoom</loc>
-    <lastmod>2024-01-29T11:55:27+00:00</lastmod>
-    <priority>0.64</priority>
+        <loc>https://gragie.arnavgupta.tech/callRoom</loc>
+        <lastmod>2024-01-29T11:55:27+00:00</lastmod>
+        <priority>0.64</priority>
 </url>
-
-
 </urlset>`;
+    return siteMap;
+};
 
-export default siteMap;
+export default page;
